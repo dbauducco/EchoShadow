3fb8d893bbc034c135f3f4cee96acfcc767eaec7
David Bauducco <hello@davidbauducco.com>
2020-12-15 23:20:04 -0600

    Lowered timeout to 50ms

--- src/repositories/EchoDataRepository.ts
+++ src/repositories/EchoDataRepository.ts
@@ -22,7 +22,7 @@ export default class EchoDataRepository {
   public async getSnapshot(): Promise<IEchoDataSnapshot | undefined> {
     try {
       const echoApiResult = await axios.get(this.apiSessionUrl, {
-        timeout: 500,
+        timeout: 50,
       });
       const snapshotData = {
         sessionId: echoApiResult.data.sessionid,
